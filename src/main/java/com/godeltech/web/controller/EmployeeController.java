e9a74fa9138dacc29791c46509fdb4f075dd2fd9
y.bakhar <egorbahar@gmail.com>
2023-04-16 09:48:13 +0300

    Add controller's methods

--- src/main/java/com/godeltech/web/controller/EmployeeController.java
+++ src/main/java/com/godeltech/web/controller/EmployeeController.java
@@ -1,13 +1,76 @@
 package com.godeltech.web.controller;
 
+import com.godeltech.component.LocalMessageSource;
+import com.godeltech.exception.AssignmentException;
+import com.godeltech.mapper.EmployeeMapper;
+import com.godeltech.persistence.model.Employee;
 import com.godeltech.service.EmployeeService;
+import com.godeltech.service.FlightCrewService;
+import com.godeltech.web.dto.request.EmployeeRequestDto;
+import com.godeltech.web.dto.response.EmployeeResponseDto;
+import liquibase.pro.packaged.E;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.util.List;
+
+@Validated
 @RestController
 @RequestMapping("/employees")
 @RequiredArgsConstructor
+@Slf4j
 public class EmployeeController {
     private final EmployeeService employeeService;
+    private final EmployeeMapper employeeMapper;
+    private final FlightCrewService flightCrewService;
+    private final LocalMessageSource localMessageSource;
+    @GetMapping("/{id}")
+    public ResponseEntity<EmployeeResponseDto> findById(@PathVariable @Validated @NotNull @Positive Long id) {
+        log.info("Find employee with id:{}", id);
+        return new ResponseEntity<>(employeeMapper.toEmployeeResponseDto(employeeService.findById(id)), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EmployeeResponseDto>> findAll() {
+        log.info("Find all categories");
+        return new ResponseEntity<>(employeeMapper.toEmployeeResponseDtoList(employeeService.findAll()), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<EmployeeResponseDto> save(@Valid @RequestBody EmployeeRequestDto employeeRequestDto) {
+        log.info("Save new employee");
+        return new ResponseEntity<>((employeeMapper.toEmployeeResponseDto(employeeService.save(employeeMapper.toEmployee(employeeRequestDto)))), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteById(@PathVariable @Validated @NotNull @Positive Long id) {
+        log.info("Delete employee with id:{}", id);
+        final boolean isAssignedToFlight = flightCrewService.findAll().stream()
+                .flatMap(flight -> flight.getEmployees().stream())
+                .map(Employee::getId)
+                .toList()
+                .contains(id);
+        if (isAssignedToFlight) {
+            throw new AssignmentException(localMessageSource.getMessage("error.record.isAssignment", new Object[]{}));
+        } else {
+            employeeService.deleteById(id);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<EmployeeResponseDto> update(@PathVariable @Validated @NotNull @Positive Long id,
+                                                      @RequestBody EmployeeRequestDto employeeRequestDto) {
+        log.info("Update employee with id:{}", id);
+        final Employee employee = employeeService.findById(id);
+        employeeMapper.updateEntity(employee,employeeRequestDto);
+        return new ResponseEntity<>(employeeMapper.toEmployeeResponseDto(employeeService.update(employee)), HttpStatus.OK);
+    }
 }
--- src/main/java/com/godeltech/web/controller/FlightCrewController.java
+++ src/main/java/com/godeltech/web/controller/FlightCrewController.java
@@ -1,13 +1,77 @@
 package com.godeltech.web.controller;
 
+import com.godeltech.component.LocalMessageSource;
+import com.godeltech.exception.AssignmentException;
+import com.godeltech.mapper.FlightCrewMapper;
+import com.godeltech.persistence.model.Category;
+import com.godeltech.persistence.model.FlightCrew;
+import com.godeltech.service.CategoryService;
 import com.godeltech.service.FlightCrewService;
+import com.godeltech.service.FlightService;
+import com.godeltech.web.dto.request.FlightCrewRequestDto;
+import com.godeltech.web.dto.response.FlightCrewResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.util.List;
+
+@Validated
 @RestController
 @RequestMapping("/flight-crews")
 @RequiredArgsConstructor
+@Slf4j
 public class FlightCrewController {
     private final FlightCrewService flightCrewService;
+    private final CategoryService categoryService;
+    private final FlightCrewMapper flightCrewMapper;
+    private final FlightService flightService;
+    private final LocalMessageSource localMessageSource;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FlightCrewResponseDto> findById(@PathVariable @Validated @NotNull @Positive Long id) {
+        log.info("Find flightCrew with id:{}", id);
+        return new ResponseEntity<>(flightCrewMapper.toFlightCrewResponseDto(flightCrewService.findById(id)), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FlightCrewResponseDto>> findAll() {
+        log.info("Find all flightCrew");
+        return new ResponseEntity<>(flightCrewMapper.toFlightCrewResponseDtoList(flightCrewService.findAll()), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<FlightCrewResponseDto> save(@Validated @RequestBody FlightCrewRequestDto flightCrewRequestDto) {
+        log.info("Save new flightCrew");
+        return new ResponseEntity<>(flightCrewMapper.toFlightCrewResponseDto(flightCrewService.save(flightCrewMapper.toFlightCrew(flightCrewRequestDto))), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteById(@PathVariable @Validated @NotNull @Positive Long id) {
+        log.info("Delete flightCrew with id:{}", id);
+        final boolean isAssignedToFlight = flightService.findAll().stream()
+                .map(flight -> flight.getFlightCrew().getId())
+                .toList()
+                .contains(id);
+        if (isAssignedToFlight) {
+            throw new AssignmentException(localMessageSource.getMessage("error.record.isAssignment", new Object[]{}));
+        } else {
+            flightCrewService.deleteById(id);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<FlightCrewResponseDto> update(@PathVariable @Validated @NotNull @Positive Long id,
+                                                        @RequestBody FlightCrewRequestDto flightCrewRequestDto) {
+        log.info("Update flightCrew with id:{}", id);
+        final FlightCrew flightCrew = flightCrewService.findById(id);
+        flightCrewMapper.updateEntity(flightCrew, flightCrewRequestDto);
+        return new ResponseEntity<>(flightCrewMapper.toFlightCrewResponseDto(flightCrewService.update(flightCrew)), HttpStatus.OK);
+    }
 }
